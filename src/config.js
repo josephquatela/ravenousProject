cba938f842183ef11cc4d12b2a7f034bbe4aa510
josephquatela <95594494+josephquatela@users.noreply.github.com>
2021-12-28 11:56:10 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    key: 'XUajUE7mKohmGQh_E8vF1_UoGbtOvRD8CEThWdZdBbKTMmksnC2yhvBenIcv2riz5PiXviPLd8DbjFLClzizoR9a6T9L9LTo9SIWU_o9BM5uKM9MiofMVA9J9jLLYXYx'
+    key: '****************'
 }
 
 export default config
